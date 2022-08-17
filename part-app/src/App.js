dcaafd809387ac0be1233ac854bc40d4aea0111e
KalynS <Kalyn@udel.edu>
2022-08-17 18:48:03 +0000

    first commit

---
+++ part-app/src/App.js
@@ -0,0 +1,12 @@
+import ParticipantForm from './Components/ParticipantForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticipantForm></ParticipantForm>
+    </div>
+  );
+}
+
+export default App;
