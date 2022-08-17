65fc262433c54c9e981ece3fae189a58c42c7a56
KalynS <Kalyn@udel.edu>
2022-08-17 19:36:42 +0000

    search filter

--- part-app/src/App.js
+++ part-app/src/App.js
@@ -1,10 +1,28 @@
-import ParticipantForm from './Components/ParticipantForm';
+// import ParticipantForm from './Components/ParticipantForm';
 import './App.css';
+import JSONDATA from "./Data.json";
+import {useState} from 'react'
 
 function App() {
+  const [searchTerm,setSearchTerm] = useState("")
   return (
     <div className="App">
-      <ParticipantForm></ParticipantForm>
+      {/* <ParticipantForm></ParticipantForm> */}
+      <input type="text" placeholder='Search...' onChange={(event) =>{setSearchTerm(event.target.value)}}/>
+     {JSONDATA.filter((val)=>{
+      if(searchTerm == ""){
+        return val
+      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+      }
+     })
+     .map((val,key)=>{
+      return(
+        <div className='user' key={key}>
+          <p>{val.name}</p>
+          </div>
+      )
+     })}
     </div>
   );
 }
